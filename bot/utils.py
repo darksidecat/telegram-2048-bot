c3b1d55a2b05acf546fe035398878d94b9f4456f
darksidecat <bitalik371@gmail.com>
2021-09-29 19:59:53 +0300

    game field table render

--- bot/utils.py
+++ bot/utils.py
@@ -1,6 +1,20 @@
 from typing import List
 
 
+def horizontal_line(
+    field: List[List[int]],
+    c_symbol: str,
+    h_symbol: str,
+    min_cell_sizes: List[int],
+):
+    line = []
+    for x in range(len(field)):
+        line.extend([c_symbol + h_symbol * min_cell_sizes[x]])
+    line.extend([c_symbol, "\n"])
+
+    return line
+
+
 def draw_table(
     field: List[List[int]],
     min_cell_size=5,
@@ -9,18 +23,29 @@ def draw_table(
     v_symbol="|",
     zero_placeholder=" ",
 ):
-    table_size = len(field)
-    min_cell_size = max(len(str(max(map(max, field)))), min_cell_size)
 
-    text = ""
-    for i, line in enumerate(field):
-        if i == 0:
-            text += (c_symbol + h_symbol * min_cell_size) * table_size + c_symbol + "\n"
-        for value in line:
-            text += v_symbol + str(zero_placeholder if value == 0 else value).center(
-                min_cell_size
+    min_cell_sizes = []
+    rotated_field = [list(row) for row in zip(*reversed(field))]
+    for row in rotated_field:
+        max_number_length = len(str(max(row)))
+        min_cell_sizes.append(max(max_number_length, min_cell_size))
+
+    text_data = []
+    for y, row in enumerate(field):
+        if y == 0:
+            text_data.extend(horizontal_line(field, c_symbol, h_symbol, min_cell_sizes))
+
+        for x, value in enumerate(row):
+            text_data.extend(
+                [
+                    v_symbol,
+                    str(zero_placeholder if value == 0 else value).center(
+                        min_cell_sizes[x]
+                    ),
+                ],
             )
-        text += v_symbol + "\n"
-        text += (c_symbol + h_symbol * min_cell_size) * table_size + c_symbol + "\n"
+        text_data.extend([v_symbol, "\n"])
 
-    return text
+        text_data.extend(horizontal_line(field, c_symbol, h_symbol, min_cell_sizes))
+
+    return "".join(text_data)
